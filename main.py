c6ede804495c9a1f600aa6ea345e056933c28eb3
Samar Balousha <2004samr@gmail.com>
2026-07-14 12:56:22 +0300

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 # 1.create an instance of FastAPI
 app = FastAPI()
 
-# 2. main route (GET /)
+# 2. create in memory database to store tasks
+
+tasks = [
+    {'id': 1, 'title': 'Task 1', 'description': 'This is task 1', 'completed': False},
+    {'id': 2, 'title': 'Task 2', 'description': 'This is task 2', 'completed': False}, 
+    {'id': 3, 'title': 'Task 3', 'description': 'This is task 3', 'completed': False}
+]
+
+# route (GET /)
 @app.get("/")
 def get_root():
     return {
@@ -12,9 +20,28 @@ def get_root():
         "endpoints": ["/tasks"]
     }
 
-# 3. health check route (GET /health)
+#  health check route (GET /health)
 @app.get("/health")
 def get_health():
     return {
         "status": "ok"
     }
+
+#   Stage 2 endpoints: read tasks 
+
+# route (GET /tasks) : get all tasks list 
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+# route (GET /tasks/{task_id}) : get a specific task by id
+@app.get("/tasks/{task_id}")
+def get_specific_task(task_id: int ):
+    for task in tasks:
+        if task['id'] == task_id:
+            return task
+    # return {"error": "Task not found"}
+
+    raise HTTPException(
+        status_code=404,
+          detail="Task not found")
